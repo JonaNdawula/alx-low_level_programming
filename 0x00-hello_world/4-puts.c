00a12b061ae4de59d7af97da36253395f466a8c1
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-15 13:32:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.hi>
-/*
-*main-Entry point
-*
-*return:Always 0
-*
-*/
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
