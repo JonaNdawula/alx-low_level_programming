781fd23e9598a5339bf7f15cabe8ba2fcd9f6d69
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-15 12:55:27 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *A program that outputs
+ Programming is like building a multilingual puzzle
+ *
+ * */
+
+ int main(void){
+ 
+ 
+ puts("programming is like building a multilingual puzzle");
+ 
+ return(0);
+ }
