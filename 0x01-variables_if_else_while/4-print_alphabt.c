e37726e0652577903d9a6478978c1fe4eefa14f9
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-16 14:28:57 -0700

    program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the letters except q and e
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int num = 0;
+
+	while (num < 26)
+	{
+		putchar(alph[num]);
+		if (num == 4 || num == 17)
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
