02b4c82f08b8c15d1884e4b2b2e2d61afcf18131
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-20 15:16:08 -0700

    unction that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int x);
 int print_sign(int y);
 int _abs(int);
 int print_last_digit(int);
-void Jack_bauer(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int,int);
 void print_to_98(int z);
