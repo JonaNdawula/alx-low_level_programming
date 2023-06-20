1f17fd4e35448da90df6920fc997ea558072fa8a
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-20 15:11:18 -0700

    unction that prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *jack_bauer - prints Jack Bauers daily minutes
+ *
+ */
+void jack_bauer(void)
+{
+int a, b;
+a = 0;
+while (a < 24)
+{
+b = 0;
+while (b < 60)
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+_putchar((':') + '0');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+_putchar("\n");
+b++;
+}
+a++;
+}
+}
