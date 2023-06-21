780189512d0ce0d4e8f85fcd390f7b9c904319e1
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-21 06:13:38 -0700

    prints the first 98 fibonacci numbers starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- *main - will print the first 98 fibonacci numbers starting with 1 and 2
+ *main - will print the first 98 fibonacci numbers starting with 1 and 2 separated
+ *by a comma and followed by a space
  *
  *Return: Always 0
  */
@@ -8,7 +9,7 @@ int main(void)
 {
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long  fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 
 for (count = 0; count < 92; count++)
@@ -20,7 +21,7 @@ fib2 = sum;
 }
 fib1_half1 = fib1 / 10000000000;
 fib2_half1 = fib2 / 10000000000;
-fib1_half1 = fib1 % 10000000000;
+fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
@@ -34,7 +35,7 @@ half2 %= 10000000000;
 }
 printf("%lu%lu", half1, half2);
 if (count != 98)
-printf(",");
+printf(", ");
 fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
 fib2_half1 = half1;
