30603f9cf3e3fae1912ecd70608440ec66322675
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-21 05:40:20 -0700

    prints the first 98 fibonacci numbers starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -19,9 +19,9 @@ fib1 = fib2;
 fib2 = sum;
 }
 fib1_half1 = fib1 / 10000000000;
-fib1_half1 = fib1 / 10000000000;
-fib2_half1 = fib2 % 10000000000;
-fib1_half2 = fib1 % 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half1 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
