c51d6a69f34f76f06868d5cc7f7cb12678901a5c
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-20 19:11:33 -0700

    a function that prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,13 +17,13 @@ if (n <= 98)
 for (a = n; a <= 98; a++)
 {
 if (a != 98)
-{
+
 printf("%d, ", a);
-}
+
 else if (a == 98)
-{
+
 printf("%d\n ", a);
-}
+
 }
 }
 else if (n >= 98)
@@ -31,13 +31,13 @@ else if (n >= 98)
 for (b = n; b >= 98; b++)
 {
 if (b != 98)
-{
+
 printf("%d, ", b);
-}
+
 else if (b == 98)
-{
+
 printf("%d\n ", b);
-}
+
 }
 }
 }
