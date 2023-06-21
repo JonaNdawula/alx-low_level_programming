7891b60006cb0b3e01c86bea646c9083775e05d1
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-21 11:03:07 -0700

    Like, comment subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 /*
  * while (i < 10)
@@ -16,7 +17,6 @@ int main(void)
  *putchar(i);
  * }
  */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
