ed6e722a12d4ef1a6ac0dd17d21dd6cfa779ee9a
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-21 04:35:16 -0700

    program which prints the sum of even fibonacci numbers less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - print the sum of even fibonacci numbers less than 4000000
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int x = 0;
+long y = 1, z = 2, sum = z;
+
+while (z + y < 400000)
+{
+z += y;
+if (z % 2 == 0)
+sum += z;
+y = z - y;
+x++;
+
+}
+printf("%ld\n", sum);
+return (0);
+}
