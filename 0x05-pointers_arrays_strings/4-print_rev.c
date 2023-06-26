22781ed8d51d4c64d5a207c3440b6d5959e942a4
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-06-26 14:05:57 -0700

    prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ *print_rev - prints a string, in reverse, followed by a new line.
+ *
+ *@s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	if (*s != '\0')
+	{
+		print_rev(s + 1);
+
+		_putchar(*s);
+
+	}
+
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string
+ *
+ *@str: numbers to print
+ *
+ */
+void puts2(char *str)
+{
+
+	if ()
+	{
+             
+	
+
+	}
+}
