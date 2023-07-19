ba0f919958abdec3cc1c75e784fa40c7beaa6470
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-07-19 08:52:14 -0700

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	int2 = atoi(argv[3]);
 
 
-	if (get_op_func(operator) == NULL || op[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
