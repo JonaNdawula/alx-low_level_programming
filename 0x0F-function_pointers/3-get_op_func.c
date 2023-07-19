61c8d0dfbe12c53756d4f79eb1a303129c06ba35
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-07-19 09:06:06 -0700

    a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,12 +14,12 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
+	
+	while ( ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s; i++)
-	{
-	}
 
 	return (ops[i].f);
 }
