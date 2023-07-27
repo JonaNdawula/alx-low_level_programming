ba15fe0885286c407537b4daec98d72a83d7b1da
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-07-27 11:26:19 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - prints sentence before main
+ *
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+void first(void)__attribute__((constructor));
 void free_list(list_t *head);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
