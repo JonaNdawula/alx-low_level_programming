f53a2d78b806da952c30b940b49c9d691a38727a
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-07-31 13:02:10 -0700

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,13 +1,79 @@
 #include "lists.h"
 /**
- *print_listint_safe -
- *@heard:
+ *print_listint_safe - prints list safely
+ *@head: points to head of list
  *
- *Return:
+ *Return:number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t nods, index = 0;
+
+	nods = loop_length(head);
+
+	if (nods == 0)
+	{
+		while (head != NULL)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			nods++;
+		}
+	}
+	else
+	{
+		while (index < nods)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			index++;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (nods);
+
+}
 
 
+/**
+ *loop_length - counts unique nodes
+ *@head: pointer to head of list
+ *
+ *Return: 0
+ */
 
+
+size_t loop_length(const listint_t *head)
+{
+
+	const listint_t *slow, *fast;
+
+	size_t nods = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+
+	for (; fast; nods++)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			for (; slow != fast; nods++)
+				slow = slow->next;
+			fast = fast->next;
+
+			slow = slow->next;
+			for (; slow != fast; nods++)
+				slow = slow->next;
+
+			return (nods);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+	return (0);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,15 +1,43 @@
 #include "lists.h"
 /**
- * free_listint_safe -
- *@h:
- * 
- *Return:
- * 
+ * free_listint_safe - frees linked list
+ *@h:points to first node in list
+ *
+ *Return: size of the list that was free’d
+ *
  */
 size_t free_listint_safe(listint_t **h)
 {
 
+	int subt;
+	size_t length;
+	list_t *temp_node;
 
+	if (h == NULL || *h == NULL)
+		return (0);
 
+	for (length = 0; *h; length++)
+	{
+		subt = *h - (*h)->next;
+
+		if (subt > 0)
+		{
+			temp_node = (*h)->next;
+			free(*h);
+			*h = temp_node;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			break;
+
+		}
+
+	}
+
+	*h = NULL;
+
+	return (length);
 
 }
