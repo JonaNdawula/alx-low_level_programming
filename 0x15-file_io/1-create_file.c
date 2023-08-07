613cc4c387dda36368c6d621488762ddba424300
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-08-07 11:33:24 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,47 @@
 #include "main.h"
 
 /**
- *create_file -
+ *create_file - a function that creates a file.
  *
- * @filename:
- * @text_content:
+ * @filename: points to file to be created
+ * @text_content:points to string to be written in file
  *
- * Return:
+ * Return:1 on success, -1 on failure
  *
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fileDef;
+	ssize_t length = 0, wr;
 
 
+	if (filename  == NULL)
+	{
+		return (-1);
+	}
+
+	fileDef = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fileDef == -1)
+	{
+		return (-1);
+	}
+
+	length = strlen(text_content);
+
+	wr = write(fileDef, text_content, length);
+
+	if (text_content != NULL)
+	{
+		if (wr == -1)
+		{
+			close(fileDef);
+
+			return (-1);
+		}
+
+	}
+
+	close(fileDef);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
