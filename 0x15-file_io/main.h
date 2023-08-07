5276c964f0cca4a73dc4e6a797f116436ce1a41b
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-08-07 13:51:07 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,12 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *newBuffer(char *nfile);
+void closeFile(int fileDesc);
 
 #endif
